59d75ed1b880ea7f3df374333c406b9d67f19574
Documenter.jl <documenter@juliadocs.github.io>
2022-07-15 21:31:46 +0000

    build based on d843763

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = UncertainHistogramming","category":"page"},{"location":"#UncertainHistogramming","page":"Home","title":"UncertainHistogramming","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for UncertainHistogramming.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [UncertainHistogramming]","category":"page"}]
+}
